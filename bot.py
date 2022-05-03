142d6c7b95eca49890900a12eefbe7924f102832
Eliot Baez <eliotbaezc@gmail.com>
2022-05-03 16:52:12 -0700

    Add basic first command
    SYN/ACK

--- bot.py
+++ bot.py
@@ -9,3 +9,18 @@ import sys
 
 
 # the code goes here
+client = commands.Bot(command_prefix='!')
+
+
+@client.event
+async def on_ready():
+    print ("SquidifyBot is now online!")
+
+
+@client.command(name = "syn")
+async def h_function(ctx, * arg):
+    await ctx.send("ack")
+
+
+if __name__ == "__main__":
+    client.run(private.token)
